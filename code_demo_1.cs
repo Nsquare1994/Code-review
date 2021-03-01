a0e6f574038ae14676d822a45bb3c2e11f0f810f
Nsquare1994 <eric97168@gmail.com>
2021-03-01 16:19:32 -0500

    Add files via upload

---
+++ code_demo_1.cs
@@ -0,0 +1,53 @@
+﻿private void LogitechSteeringWheelBtnPress()
+{
+    LogitechGSDK.DIJOYSTATE2ENGINES rec;
+    rec = LogitechGSDK.LogiGetStateUnity(0);
+
+    if (rec.rglSlider[0] != 32767)
+    {
+        //Debug.Log(this.transform.position);
+        // right
+        // change gear)
+
+        if (LogitechGSDK.LogiButtonTriggered(0, 4))
+        {
+            if (this.gearStatus >= 3)
+                this.gearStatus = 0;
+            else
+                this.gearStatus++;
+        }
+        // left
+        // change gear
+        else if (LogitechGSDK.LogiButtonTriggered(0, 5))
+        {
+            if (this.gearStatus <= 0)
+                this.gearStatus = 3;
+            else
+                this.gearStatus--;
+        }
+    }
+
+    // 0 = P, 1 = N, 2 = R, 3 = D
+    if (gearStatus == 0)
+        shiftGear.text = "P";
+    else if (gearStatus == 1)
+        shiftGear.text = "N";
+    else if (gearStatus == 2)
+        shiftGear.text = "R";
+    else if (gearStatus == 3)
+        shiftGear.text = "D";
+    if (rec.rgbButtons[10] == 128)
+    {
+        // right
+        // right turn light
+    }
+    if (rec.rgbButtons[11] == 128)
+    {
+        // left
+        // left turn light
+    }
+    if (rec.rgbButtons[23] == 128)
+    {
+        // horn
+    }
+}
---
+++ code_demo_2.cs
@@ -0,0 +1,21 @@
+﻿private void Steer()
+{
+    if (LogitechGSDK.LogiIsConnected(0))
+    {
+        LogitechGSDK.DIJOYSTATE2ENGINES rec;
+
+        rec = LogitechGSDK.LogiGetStateUnity(0);
+        // for wheel object rotate
+        float degree = -1.0f * (float)(rec.lX / 32767.0f) * 450.0f;
+        if (this.SteerWheel)
+        {
+            this.SteerWheel.transform.eulerAngles = new Vector3(this.SteerWheel.transform.eulerAngles.x, this.SteerWheel.transform.eulerAngles.y, degree);
+        }
+        // for steer object rotate
+        this.FLWheelC.steerAngle = this.maxSteerAngle * rec.lX / 32767;
+        this.FRWheelC.steerAngle = this.maxSteerAngle * rec.lX / 32767;
+        LogitechGSDK.LogiPlayDamperForce(0, 50);
+        LogitechGSDK.LogiPlaySpringForce(this.index, this.offsetPercentage, this.saturationPercentage, this.coefficientPercentage);
+    }
+
+}
